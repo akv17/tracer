0acb0ee1dae2251eb4d7892b6cab5b6a0b1096bb
akv17 <temakep@list.ru>
2020-09-17 00:45:41 +0300

    add more vars

--- tests/test_proj/main.py
+++ tests/test_proj/main.py
@@ -6,7 +6,7 @@ from tests.test_proj.bar import bar, buzz
 foo_obj = Foo()
 
 
-def main(x):
+def main(x, y=-1, z=(None,)):
     x = foo_obj.foo(x)
     x = bar(x)
     x = foo(x)
@@ -14,4 +14,5 @@ def main(x):
     x = foo_obj(x)
     # just to test correct binding of staticmethods's sigs.
     foo_obj.bar(0x0)
+    unused = 0x0
     return x
