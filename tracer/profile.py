ee51d091256242f5a2e2bc3873416f80eb104e84
akv17 <artem@nlogic.ai>
2022-01-28 22:06:22 +0300

    compute module name

--- tracer/profile.py
+++ tracer/profile.py
@@ -1,3 +1,5 @@
+import os
+import inspect
 import cProfile
 import pstats
 from dataclasses import dataclass, field
@@ -7,14 +9,27 @@ from typing import Any
 @dataclass(frozen=True)
 class Function:
     name: str
+    module: str
     file: str = field(repr=False)
     line: int = field(repr=False)
+    num_calls: int = field(repr=False)
+    _runtime_inner: float = field(repr=False)
     callers: Any = field(repr=False, default_factory=list)
     callees: Any = field(repr=False, default_factory=list)
-    _runtime_inner: float = field(default=None, repr=False)
 
-    def set_inner_runtime(self, value):
-        object.__setattr__(self, '_runtime_inner', value)
+    @property
+    def id(self):
+        return self.file, self.line, self.name
+
+    @property
+    def runtime(self):
+        value = self._runtime_inner + sum([c.runtime for c in self.callees] or [0])
+        return value
+
+    @property
+    def qualname(self):
+        rv = self.name if self.module == '<builtins>' else f'{self.module}.{self.name}'
+        return rv
 
     def add_caller(self, call):
         self.callers.append(call)
@@ -22,17 +37,12 @@ class Function:
     def add_callee(self, call):
         self.callees.append(call)
 
-    @property
-    def runtime(self):
-        value = self._runtime_inner + sum([c.runtime for c in self.callees] or [0])
-        return value
-
 
 @dataclass(frozen=True)
 class Call:
     src: Function
     dst: Function
-    num: int
+    num_calls: int
     runtime: float
 
 
@@ -41,51 +51,78 @@ class Stats:
     def __init__(self, funcs):
         self._funcs = funcs
 
+    def list_funcs(self):
+        return self._funcs
 
-def _create_stats_from_profiler(profiler):
-    stats = pstats.Stats(profiler).stats  # noqa
-    funcs = {}
-    for dst, (_, nc, tt, ct, callers) in stats.items():
-        if dst not in funcs:
-            file, line, name = dst
+
+class _CreateStatsCommand:
+
+    def __init__(self, profiler, fs_root):
+        self.profiler = profiler
+        self.fs_root = fs_root
+        self.pstats = pstats.Stats(profiler).stats  # noqa
+
+    def __call__(self):
+        funcs = self._create_funcs()
+        funcs = self._set_calls(funcs)
+        stats = Stats(funcs)
+        return stats
+
+    def _create_module_name(self, file):
+        if file == '~':
+            mod = '<builtins>'
+        elif self.fs_root not in file:
+            # TODO: figure out real module
+            mod = '<packages>'
+        else:
+            mod = file.replace(self.fs_root, '')
+            mod = mod.lstrip(os.path.sep)
+            mod = mod.replace('.py', '')
+            mod = '.'.join(mod.split(os.path.sep))
+        return mod
+
+    def _create_funcs(self):
+        funcs = []
+        for id_, (_, nc, tt, _, callers) in self.pstats.items():
+            file, line, name = id_
+            mod_name = self._create_module_name(file)
             func = Function(
                 name=name,
                 file=file,
                 line=line,
+                num_calls=nc,
+                module=mod_name,
+                _runtime_inner=tt
             )
-            funcs[dst] = func
-        func_dst = funcs[dst]
-        func_dst.set_inner_runtime(tt)
-        for src, (_, nc_, tt_, ct_) in callers.items():
-            if src not in funcs:
-                file, line, name = src
-                func = Function(
-                    name=name,
-                    file=file,
-                    line=line,
+            funcs.append(func)
+        return funcs
+
+    def _set_calls(self, funcs):
+        funcs_map = {f.id: f for f in funcs}
+        for dst_id, (*_, callers) in self.pstats.items():
+            func_dst = funcs_map[dst_id]
+            for src_id, (_, nc_, _, ct_) in callers.items():
+                func_src = funcs_map[src_id]
+                call = Call(
+                    src=func_src,
+                    dst=func_dst,
+                    runtime=ct_,
+                    num_calls=nc_,
                 )
-                funcs[src] = func
-            func_src = funcs[src]
-            call = Call(
-                src=func_src,
-                dst=func_dst,
-                runtime=ct_,
-                num=nc_,
-            )
-            func_src.add_callee(call)
-            func_dst.add_caller(call)
-    funcs = list(funcs.values())
-    obj = Stats(funcs)
-    return obj
+                func_src.add_callee(call)
+                func_dst.add_caller(call)
+        return funcs
 
 
 class Profile:
 
     def __init__(self):
+        self.stats = None
         self._profiler = None
-        self._stats = None
+        self._fs_root = None
 
     def __enter__(self):
+        self._set_fs_root()
         self._profiler = cProfile.Profile()
         self._profiler.enable()
         return self
@@ -93,6 +130,19 @@ class Profile:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._profiler.disable()
         if exc_type is None:
-            self._stats = _create_stats_from_profiler(self._profiler)
+            self.stats = _CreateStatsCommand(
+                profiler=self._profiler,
+                fs_root=self._fs_root
+            )()
         self._profiler = None
+        self._fs_root = None
         return False
+
+    def _set_fs_root(self):
+        # frames:
+        #   0: here
+        #   1: self.__enter__
+        #   2: Profile entry point
+        caller = inspect.stack()[2]
+        root, _ = os.path.split(caller.filename)
+        self._fs_root = root
