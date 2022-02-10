ae7e7dc673bc51a3e862042e23633f0e370a399b
akv17 <artem@nlogic.ai>
2022-02-11 00:25:42 +0300

    wip: minmal trace app draft

--- app/trace_.py
+++ app/trace_.py
@@ -16,6 +16,7 @@ class Model:
             with Trace() as trace:
                 with open(self.script, 'r') as f:
                     code = f.read()
+                    code = compile(code, self.script, 'exec')
                     exec(code, {'__name__': '__main__'})
             self._traces = trace.traces
         return self._traces
@@ -24,10 +25,107 @@ class Model:
 class View:
 
     def __init__(self):
-        self.traces = None
+        self._traces = None
+        self._func = None
+        self._call = 0
+        self._line = 0
+        self._calls_map = None
+
+    def _on_click_decr_call(self):
+        self._call -= 1
+        self._call = max(0, self._call)
+
+    def _on_click_incr_call(self):
+        self._call += 1
+
+    def _on_click_decr_line(self):
+        self._line -= 1
+        self._line = max(0, self._line)
+
+    def _on_click_incr_line(self):
+        self._line += 1
+
+    def _render_menu(self):
+        self._func = st.selectbox('Function', options=list(self._calls_map))
+
+    def _render_controls(self):
+        st.write('_' * 10)
+        col0, col1, *_ = st.columns(10)
+        with col0:
+            st.button('Previous Call', on_click=self._on_click_decr_call, key='pc')
+        with col1:
+            st.button('Next Call', on_click=self._on_click_incr_call, key='nc')
+        col0, col1, *_ = st.columns(10)
+        with col0:
+            st.button('Previous Line', on_click=self._on_click_decr_line, key='pln')
+        with col1:
+            st.button('Next Line', on_click=self._on_click_incr_line, key='nln')
+        st.write('_' * 10)
+
+    def _render_call(self):
+        calls = self._calls_map[self._func]
+        try:
+            call = calls[self._call]
+        except IndexError:
+            call = calls[-1]
+            self._call = len(calls) - 1
+        col0, col1 = st.columns(2)
+        with col0:
+            st.subheader('Stack')
+            stack = call.stack
+            size = len(stack)
+            stack = [f'{size - i}. -> {f}' for i, f in enumerate(stack)]
+            stack = '\n'.join(stack)
+            st.code(stack)
+        with col1:
+            st.subheader('Info')
+            st.table([
+                ['func', call.func],
+                ['call#', f'{self._call+1}/{len(calls)}'],
+                ['line#', f'{self._line+1}/{len(call.lines)}'],
+                ['file', call.file],
+            ])
+
+    def _render_line(self):
+        calls = self._calls_map[self._func]
+        call = calls[self._call]
+        lines = call.lines
+        try:
+            line = lines[self._line]
+        except IndexError:
+            line = lines[-1]
+            self._line = len(lines) - 1
+        prev_lines = lines[max(0, self._line - 5):self._line]
+        next_lines = lines[self._line+1:self._line + 5 + 1]
+        col0, col1 = st.columns(2)
+        with col0:
+            st.subheader('Context')
+            ctx = [f'def {line.func}:']
+            for ln in prev_lines:
+                text = f'    {ln.lineno}\t{ln.get_src()}'.rstrip()
+                ctx.append(text)
+            ctx.append(f'->  {line.lineno}\t{line.get_src()}'.rstrip())
+            for ln in next_lines:
+                text = f'    {ln.lineno}\t{ln.get_src()}'.rstrip()
+                ctx.append(text)
+            ctx = '\n'.join(ctx)
+            st.code(ctx)
+        with col1:
+            st.subheader('Vars')
+            st.write(line.vars)
+
+    def set_traces(self, traces):
+        calls_map = {}
+        for call in traces:
+            calls_map.setdefault(call.func, []).append(call)
+        self._calls_map = calls_map
+        self._traces = traces
 
     def run(self):
-        st.write(self.traces)
+        self._render_menu()
+        self._render_controls()
+        self._render_call()
+        self._render_line()
 
 
 class App:
@@ -39,7 +137,7 @@ class App:
     def run(self, script):
         self.model.script = script
         traces = self.model.run()
-        self.view.traces = traces
+        self.view.set_traces(traces)
         self.view.run()
 
 
